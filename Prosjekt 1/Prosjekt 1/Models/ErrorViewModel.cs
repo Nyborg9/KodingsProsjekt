0574434403c804f2e02c4d103a636f20a451b8d3
Nyborg9 <144025186+Nyborg9@users.noreply.github.com>
2024-09-23 01:24:33 +0200

    Delete Prosjekt 1 directory

--- Prosjekt 1/Prosjekt 1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Prosjekt_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
