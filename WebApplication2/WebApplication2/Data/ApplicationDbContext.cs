abeb193aaac76af9c565f4f044d9e0482948a7e5
Nyborg9 <mikaelnyb@gmail.com>
2024-10-28 00:05:11 +0100

    Prøver å legge til database

---
+++ WebApplication2/WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet for GeoChange
+        public DbSet<GeoChange> GeoChanges { get; set; }
+    }
+}
