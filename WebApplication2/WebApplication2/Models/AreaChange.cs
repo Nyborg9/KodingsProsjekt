93699e87ef1c91a2dfc439a7828edf83d83f5b76
NinaGundersen <ninamg18@student.uia.no>
2024-11-01 13:14:13 +0100

    fikset overview og areachange

--- WebApplication2/WebApplication2/Models/AreaChange.cs
+++ WebApplication2/WebApplication2/Models/AreaChange.cs
@@ -7,17 +7,6 @@
         //GeoJSON format - markører, linjer, polygoner
         public string GeoJson { get; set; }  
         public string Description { get; set; }
-
-        public string MapVariant { get; set; }
-        public Status Status { get; set; } // Store as enum type
-    }
-
-    public enum Status
-    {
-        Innsendt,
-        UnderVurdering,
-        IkkeTattTilFølge,
-        Godkjent,
     }
 }
 
