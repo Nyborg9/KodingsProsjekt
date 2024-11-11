b26d9a0e60eb7632a388b2cfbae366b5b8d14b64
ZG-RY0Ka <shayan-shahid@outlook.com>
2024-11-11 09:16:20 +0100

    added button

---
+++ WebApplication2/WebApplication2/Models/PositionModels.cs
@@ -0,0 +1,11 @@
+namespace WebApplication2.Models
+{
+
+        public class PositionModels
+        {
+                public string Latitude { get; set; }
+                public string Longitude { get; set; }
+                public string Description { get; set; }
+
+        }
+}
