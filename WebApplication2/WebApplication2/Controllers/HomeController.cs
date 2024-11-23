02e24a947d034aaffc955a086ad44a66ad0469fb
Mikael <mikaelnyb@gmail.com>
2024-11-23 13:17:22 +0100

    Slettet ekstra kode

--- WebApplication2/WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -5,16 +5,10 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger; // Add ILogger
-        private readonly ApplicationDbContext _context; // Add ApplicationDbContext
-        private readonly UserManager<ApplicationUser> _userManager; // Add UserManager
 
         // Initialize Logger, context and UserManager
-        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
+        public HomeController()
         {
-            _logger = logger;
-            _context = context;
-            _userManager = userManager;
         }
 
         public IActionResult Index()
